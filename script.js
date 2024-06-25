f3fca4831522d9449433f32214c7becfc725a70a
John Ratcliffe <2095497@students.wits.ac.za>
2024-06-25 17:32:58 +0200

    Added model with 80% acc

--- script.js
+++ script.js
@@ -1,9 +1,89 @@
-let results
-const csvData = Papa.parse('/train.csv', {
-  download: true,
-  header: true,
-  complete: function (data) {
-    results = data.data
-    console.log(results)
+const rawFile = '/train.csv'
+const parseFile = rawFile => {
+  return new Promise(resolve => {
+    Papa.parse(rawFile, {
+      download: true,
+      dynamicTyping: true,
+      complete: results => {
+        resolve(results.data)
+      }
+    })
+  })
+}
+const csv = await parseFile(rawFile)
+
+const INPUTS = []
+const OUTPUTS = []
+let i
+for (i = 1; i < csv.length; i++) {
+  INPUTS.push(csv[i].slice(1, 785))
+  OUTPUTS.push(csv[i][0])
+}
+tf.util.shuffleCombo(INPUTS, OUTPUTS)
+const INPUTS_TENSOR = tf.tensor2d(INPUTS)
+const OUTPUTS_TENSOR = tf.oneHot(tf.tensor1d(OUTPUTS, 'int32'), 10)
+
+const model = tf.sequential()
+model.add(tf.layers.dense({ inputShape: [784], units: 32, activation: 'relu' }))
+model.add(tf.layers.dense({ units: 16, activation: 'relu' }))
+model.add(tf.layers.dense({ units: 10, activation: 'softmax' }))
+model.summary()
+train()
+
+async function train () {
+  model.compile({
+    optimizer: 'adam',
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy']
+  })
+
+  const results = await model.fit(INPUTS_TENSOR, OUTPUTS_TENSOR, {
+    shuffle: true,
+    validationSplit: 0.2,
+    batchSize: 512,
+    epochs: 50,
+    callbacks: { onEpochEnd: logProgress }
+  })
+
+  OUTPUTS_TENSOR.dispose()
+  INPUTS_TENSOR.dispose()
+
+  evaluate()
+}
+
+function logProgress (epoch, logs) {
+  console.log('Data for epoch ' + epoch, logs)
+}
+
+const PREDICTION_ELEMENT = document.getElementById('prediction')
+
+function evaluate () {
+  const OFFSET = Math.floor((Math.random() * INPUTS.length))
+  const answer = tf.tidy(function () {
+    const newInput = tf.tensor1d(INPUTS[OFFSET]).expandDims()
+    const output = model.predict(newInput)
+    output.print()
+    return output.squeeze().argMax()
+  })
+
+  answer.array().then(function (index) {
+    PREDICTION_ELEMENT.innerText = index
+    PREDICTION_ELEMENT.setAttribute('class', (index === OUTPUTS[OFFSET]) ? 'correct' : 'wrong')
+    answer.dispose()
+    drawImage(INPUTS[OFFSET])
+  })
+}
+
+const CANVAS = document.getElementById('canvas')
+const CTX = CANVAS.getContext('2d')
+function drawImage (digit) {
+  const imageData = CTX.getImageData(0, 0, 28, 28)
+  for (let i = 0; i < digit.length; i++) {
+    imageData.data[i * 4] = digit[i] * 255
+    imageData.data[i * 4 + 1] = digit[i] * 255
+    imageData.data[i * 4 + 2] = digit[i] * 255
+    imageData.data[i * 4 + 3] = 255
   }
-})
+  CTX.putImageData(imageData, 0, 0)
+  setTimeout(evaluate, 2000)
+}
